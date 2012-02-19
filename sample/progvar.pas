b6c035e4e3ceee9c627c9a7e35d49ba0f0d8282f
Marcin <marman1@gmail.com>
2012-02-19 15:05:35 +0000

    test

--- sample/progvar.pas
+++ sample/progvar.pas
@@ -1,33 +1,10 @@
 program owned;
 
-var 	A, B, C, D : INTEGER;
+var 	A, B : INTEGER;
 
 BEGIN
-	A := 1;
-	B := 2;
-	C := 3;
-	D := 4;
-	
-	IF  A > 0  THEN B := 1 ELSE B := 2;
-	
-
-	IF (B = 1) THEN
-	BEGIN
-		C := 4;
-		D := 5;
-	END
-	ELSE
-	BEGIN
-		C := 7;
-		D := 8;
-	END;
-
-	B := 0;
-	C := 0;
-
 	FOR A := 1 TO 5 DO
 	BEGIN
-		B := B+A;
-		C := C+A+A;
+		IF A = 2 THEN B := 4;
 	END;
 END.
